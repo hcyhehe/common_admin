a1ae32d46a39d551ec022dfab3a7046b071a0104
hcyhehe <960696163@qq.com>
2020-06-28 23:39:08 +0800

    666

--- src/myviews/genecode/add.vue
+++ src/myviews/genecode/add.vue
@@ -1,31 +1,49 @@
 <template>
-  <div class="qna">
-    <el-form ref="form" label-width="80px">
-      <el-form-item label="排序">
-        <el-input v-model.trim="params.sort" placeholder="请输入排序" style="width:200px;" clearable></el-input>
-      </el-form-item>
-      <el-form-item label="问题分类">
-        <el-input v-model.trim="params.name" placeholder="请输入问题分类" style="width:200px;" clearable></el-input>
-      </el-form-item>
-      <el-form-item label="图标">
-        <el-upload
-          class="upload-demo"
-          :limit="limit"
-          :action="upload"
-          :on-remove="handleRemove"
-          :on-exceed="handleExceed"
-          :on-success="handleSuccess"
-          :file-list="fileList"
-          list-type="picture">
-          <el-button size="small" type="primary">点击上传</el-button>
-          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过2M</div>
-        </el-upload>
-      </el-form-item>
+  <div class="genecode">
+    <div class="filter-container">
+      <el-button class="filter-item" type="primary" icon="el-icon-circle-plus-outline" @click="addLine">添加</el-button>
+    </div>
 
+    <el-form :inline="true" label-width="65px" v-for="(obj,index) in data" :key="obj+index">
+      <el-form-item label="字段名:">
+        <el-input class="geInput" v-model="obj.name" placeholder="字段名" />
+      </el-form-item>
+      <el-form-item label="类型:">
+        <el-select class="geInput" v-model="obj.type" placeholder="请选择">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="长度:">
+        <el-input class="geInput2" v-model="obj.length" placeholder="长度" />
+      </el-form-item>
+      <el-form-item label="小数点:">
+        <el-input class="geInput2" v-model="obj.decimal" placeholder="小数点" />
+      </el-form-item>
+      <el-form-item label="为空:">
+        <el-checkbox v-model="obj.isNull"></el-checkbox>
+      </el-form-item>
+      <el-form-item label="默认值:">
+        <el-input class="geInput2" v-model="obj.default" placeholder="默认值" />
+      </el-form-item>
+      <el-form-item label="主键:">
+        <el-checkbox v-model="obj.mkey"></el-checkbox>
+      </el-form-item>
+      <el-form-item label="自增:">
+        <el-checkbox v-model="obj.autoIncre"></el-checkbox>
+      </el-form-item>
+      <el-form-item label="备注:">
+        <el-input class="geInput" v-model="obj.remark" placeholder="备注" />
+      </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">提交</el-button>
+        <el-button type="danger" @click="del(index)">删除</el-button>
       </el-form-item>
     </el-form>
+
+    <div class="filter-container submit">
+      <el-button class="filter-item" type="primary" icon="el-icon-upload" @click="onSubmit">提交</el-button>
+    </div>
+    
   </div>
 </template>
 
@@ -40,52 +58,43 @@ export default {
   data(){
     return {
       userInfo: {},
-      params: {
-        sort: '',
-        name: '',
-        icon: '',
-      },
-      upload: base.upload,
-      fileList: [],
-      limit: 1,
+      data:[
+        { name:'', type:'', length:'', decimal:'', isNull:false, default:'', mkey:false, autoIncre:false, remark:'' },
+      ],
     }
   },
   methods:{
+    addLine(){
+      const obj = { name:'', type:'', length:'', decimal:'', isNull:false, default:'', mkey:false, autoIncre:false, remark:'' };
+      this.data.push(obj);
+    },
+
+    del(index){
+      this.data.splice(index, 1);
+    },
+
     onSubmit(){
       let that = this;
-      console.log('this.params', this.params);
-      for(let i in this.params){
-        if(!this.params[i]){
-          return this.$message.warning('填写不完整');
-        }
-      }
-      if(!judgeNum1(this.params.sort)){
-        return this.$message.warning('排序不是非负整数');
-      }
-      aPost(base.questionAdd, that.params).then(res=>{
-        if(res.data.code==2000000){
-          that.$message.success('提交成功');
-          that.$router.push({path:'/question/list'});
-        } else {
-          that.$message.warning(res.data.msg);
-        }
-      }).catch(err=>{
-        console.log(err);
-      })
+      console.log('this.data', JSON.stringify(this.data));
+
+      // aPost(base.questionAdd, that.params).then(res=>{
+      //   if(res.data.code==2000000){
+      //     that.$message.success('提交成功');
+      //     that.$router.push({path:'/question/list'});
+      //   } else {
+      //     that.$message.warning(res.data.msg);
+      //   }
+      // }).catch(err=>{
+      //   console.log(err);
+      // });
     },
-    handleRemove(file, fileList){
-      this.params.icon = '';
-    },
-    handleExceed(file, fileList){
-      this.$message.warning('只能上传一张图片，请删除后再重新上传');
-    },
-    handleSuccess(res, file, fileList){
-      this.params.icon = res.fileurl;
-    },
+
   },
+
   created(){
     this.userInfo = JSON.parse(localStorage.getItem('st_userinfo'));
   },
+
   mounted(){
     
   }
@@ -93,8 +102,23 @@ export default {
 </script>
 
 <style lang="scss">
-.qna{
-  width: 1000px;
+.genecode{
   padding: 30px 30px;
+  .geInput{
+    input{
+      width: 130px;
+    }
+  }
+  .geInput2{
+    input{
+      width: 80px;
+    }
+  }
+  .el-input-number--medium{
+    width: 130px;
+  }
+  .submit{
+    margin-top: 20px;
+  }
 }
 </style>
